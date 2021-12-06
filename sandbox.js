d04eaffa63c0845c89774bf34f53112c1913d415
hr_hassan_raza <hraza20000@gmail.com>
2021-12-06 23:19:32 +0500

    day-one

---
+++ sandbox.js
@@ -0,0 +1,131 @@
+// alert("Hello World");
+//let age = 25; // can be chnaged later on
+//const points = 100; // can't be changed
+
+// strings
+//let email = "Hassan raza";
+/*
+let firstName = "Hassan";
+let lastName = "Raza";
+let fullName = firstName + " " + lastName;
+a = fullName[0];
+let length = fullName.length;
+let upperCase = fullName.toUpperCase(); // toLowerCase
+let letter = fullName.indexOf("R");
+let result = fullName.slice(0, 5); // give positions in agruments
+// let result = fullName.substr(startPosition, how many characters);
+let resultN = fullName.replace("n", "w"); // replaces first if multiple characters
+let result = email.inculdes('@');
+
+console.log(resultN);
+*/
+
+// numbers math operators +,-,*,/,**,%
+// ++, --
+// NaN - not a number
+
+// tempelate strings
+
+/*
+const title = "Best Roads of 2021";
+const author = "Hassan";
+const likes = 30;
+
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
+let html = `
+<h2> ${title}</h2>
+<p> By ${author}</p>
+<span>This blog has ${likes} likes </span>
+`;
+console.log(html);
+*/
+
+// Arrays and objects
+
+/*
+let ninjas = ["shaun", "ryu", "chun-li"];
+let ages = [20, 30, 35];
+
+//console.log(ninjas.length);
+
+//let result = ninjas.join(","); // joining using delimeter as ,
+
+//let result = ninjas.indexOf("ryu");
+
+// let result = ninjas.concat(ages);
+
+//let result = ninjas.push("Ken"); // returns length after adding one element in array
+
+let result = ninjas.pop(); //poping last element
+
+console.log(result);
+*/
+//let age = 25
+//console.log(age!=25)  console.log(age!='25') // both are same
+
+// strict comparison
+
+//console.log(age===25)  console.log(age==='25') // both are not same
+
+//!== // strcit comparison
+
+// type conversion
+
+/*
+let score = "100";
+score = Number(score);
+console.log(score + 1);
+console.log(typeof score);
+*/
+
+// for (let i = 0; i < 5; i++)
+/*
+const names = ["Hassan", "Raza", "Chandio"];
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+  let html = `<div>${names[i]}</div>`;
+  console.log(html);
+}
+*/
+/*
+while (i < 5) {
+  console.log("in loop ", i);
+}
+*/
+
+/*
+do 
+{
+    console.log('val of i is', i)
+}while(i < 5);
+*/
+
+/// follows C++ syntax
+/*
+const password = "pass";
+if (password.length >= 8 && password.includes("@")) {
+  console.log("Hassan Raza");
+} else if (password.length > 10) {
+  console.log("Password is not long enough");
+} else {
+  console.log("Hassan");
+}
+*/
+
+// continue and break works same like C++ in loops
+
+// switch statements
+/*
+const grade = "D";
+switch (grade) {
+  case "A":
+    console.log("You got A");
+  case "B":
+    console.log("You got B");
+  case "D":
+    console.log("You got D");
+  default:
+    console.log("Invalid Grade");
+}
+*/
